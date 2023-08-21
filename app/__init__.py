15b8e00e2845b2307619d0e6dcdd6d7bd02e6d15
will-mcintyre04 <william.d.j.mcintyre@gmail.com>
2023-08-20 23:13:43 -0400

    comments

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def create_app(config_name):
+
     app = Flask(__name__)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('../instance/config.py')
+
+    db.init_app(app) #Connect SQLAlchemy database ORM to the flask app
+    with app.app_context():
+        db.create_all()#This function scans the defined models (Python classes) that inherit from db.Model and generates the SQL statements required to create the corresponding database tables
 
     from .routes import bp
     app.register_blueprint(bp)
--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from app import create_app
+from dotenv import load_dotenv
+
+## Load environemtn variables!
+load_dotenv()
 
 if __name__ == '__main__':
     app = create_app('development')
