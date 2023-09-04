796a75408f68f232330fa725029f931b976c9eaa
will-mcintyre04 <william.d.j.mcintyre@gmail.com>
2023-09-04 04:14:31 +0000

    updated for pythonanywhere support

--- app/models.py
+++ app/models.py
@@ -28,6 +28,7 @@ class Email(db.Model):
         Example: "user@example.com"
 
     """
+    __tablename__ = "emails"
 
     id = db.Column(db.Integer, primary_key=True)
     address = db.Column(db.String(120), unique=True, nullable=False)
